d0bab1339a0ba1ade9fb8e65751386fb76dac6ef
ly <queeniiee02@gmail.com>
2023-11-17 02:41:50 +0800

    add doxygen notation example in main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,22 @@
 #include "fitness.hpp"
 #include "gp.hpp"
 #include "generate_train_data.hpp"
+/**
+ * Doxygen comments go here.
+ *
+ * The extra asterisks to the left are not required, but they are nice to have
+ * visually.
+ */
 
+/**
+ * \brief The main function.
+ *
+ * This function run the gp.
+ * 
+ * \param argc The number of arguments.
+ * \param argv The vector of arguments.
+ * \return The exec result of main function.
+ */
 int main(int argc, char** argv) {
     freopen( "error.txt", "w", stderr );
     generate_train_data();

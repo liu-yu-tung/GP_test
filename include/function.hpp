12eef33a7c0dcdacf2d9206e9c849f5cedbfd9b1
liuyu <queeniiee02@gmail.com>
2023-11-23 20:54:49 +0800

    modify function.hpp and function.cpp

--- include/function.hpp
+++ include/function.hpp
@@ -9,96 +9,30 @@ class Function {
 friend class Program;
 public:
     Function(Data* dataPtr);
+
     void addChild(Function *f);
     void show();
     void setHeight(int h);
+    void setInRecursive(bool state);
+    void setListBegin(int begin);
+
     virtual std::string getFunctionName() = 0;
     virtual void execution() = 0;
+    virtual int getArity() = 0;
 
     Data *dataPtr;
-    static const int arity;
-    static const int outputNumber;
-    int height;
-
-    virtual int getArity() = 0;
-    virtual int getOutputNumber() = 0;
     std::vector<Function*> children;
+
     bool inRecursive;
     int listBegin;
     int height;
-    DataType outType;
-    std::vector<DataType> inType;
-};
 
-/**
-class Function{
-friend class Program;
-public:
-    Function(Data *);
-    void addChild(Function *);
-    void show();
-    void setHeight(int);
-    int& getInt(std::vector<int> &, int);
-    float& getFloat(std::vector<int> &, int);
-
-    virtual std::string getFunctionName() = 0;
-    virtual void execution() = 0;
-
-    virtual int getArity() = 0;
-    virtual int getOutputNumber() = 0;
-
-    /**
-     * For 1-D data, store index once a time. e.g, argumentIndex[0] = index.
-     * For 2-D data, store index twice a time. argumentIndex[0] & argume.ntIndex[1] = index.
-     * 
-    */
-    std::vector<int> argumentIndex;
-    std::vector<int> outputIndex;
-
-protected:
-    Data *dataPtr;
     static const int arity;
-    static const int outputNumber;
-    int height;
-    std::vector<Function *> children;
-public:
-    void setArgument(std::vector<int> &, std::vector<int> &);
-
+    static const std::vector<DataType> inType;
+    static const DataType outType;
 };
 
-class Leaf: public Function{
-public:
-    Leaf(Data *);
-    static const std::string functionName;
-    std::string getFunctionName() override;
-    void execution() override;
-    int getArity() override;
-    int getOutputNumber() override; 
-protected:
-    static const int arity;
-    static const int outputNumber;
 
-};
-
-/**
- * \brief Define Max2 Fucntion class.
-*/
-/*
-class Max2: public Function{
-public:
-    Max2(Data *);
-    static const std::string functionName;
-    std::string getFunctionName() override;
-    void execution() override;
-    int getArity() override;
-    int getOutputNumber() override; 
-protected:
-    static const int arity;
-    static const int outputNumber;
-
-};
-
-*/
 class Swap: public Function{
 public:
     Swap(Data *);
@@ -106,11 +40,11 @@ public:
     std::string getFunctionName() override;
     void execution() override;
     int getArity() override;
-    int getOutputNumber() override;
-protected:
 
+protected:
     static const int arity;
-    static const int outputNumber;
+    static const std::vector<DataType> inType;
+    static const DataType outType;
 };
 
 /**
@@ -127,13 +61,12 @@ public:
     std::string getFunctionName() override;
     void execution() override;
     int getArity() override;
-    int getOutputNumber() override;
+    //int getOutputNumber() override;
 protected:
     std::unique_ptr<Function> f;
     std::unique_ptr<Function> g;
     static const int arity;
-    static const int outputNumber;
-
+    //static const int outputNumber;
 };
 
 
--- src/function.cpp
+++ src/function.cpp
@@ -1,44 +1,20 @@
 #include "function.hpp"
 
 const int Function::arity = -1;
-const int Function::outputNumber = -1;
 
-<<<<<<< HEAD
-/*
-=======
-const int Leaf::arity = 0;
-const int Leaf::outputNumber = 0;
-const std::string Leaf::functionName = "Leaf";
-
->>>>>>> 7ca8db6e4d5283a3d923aa3a2621748d4be32fc6
-const int Max2::arity = 2;
-const int Max2::outputNumber = 1;
-const std::string Max2::functionName = "Max2";
-*/
+// Swap
 const int Swap::arity = 2;
-const int Swap::outputNumber = 0;
 const std::string Swap::functionName = "Swap";
+const std::vector<DataType> Swap::inType = {DataType::Int, DataType::Int};
+const DataType Swap::outType = DataType::None;
+
+
+// Loop
 const int Loop::arity = 0;
-const int Loop::outputNumber = 0;
 const std::string Loop::functionName = "Loop";
 
 Function::Function(Data* dataPtr_): dataPtr(dataPtr_){};
 
-/*
-void Function::setArgument(std::vector<int> &arguments, std::vector<int> &outputs){
-    if(arguments.size()!=getArity()*Const::dimensionOfBuffer){
-        std::cerr << getFunctionName() << " should get " << getArity()*Const::dimensionOfBuffer<< " arguments\n";
-        return;
-    } 
-    if(outputs.size()!=getOutputNumber()*Const::dimensionOfBuffer){
-        std::cerr << getFunctionName() << " should get " << getOutputNumber()*Const::dimensionOfBuffer<< " arguments\n";
-        return;
-    } 
-    
-    argumentIndex = arguments;
-    outputIndex = outputs;
-};
-*/
 
 void Function::addChild(Function *child){
     children.push_back(child);
@@ -65,60 +41,13 @@ void Function::setHeight(int h){
     height = h;
 };
 
-/*
-int &Function::getInt(std::vector<int> &index, int order){
-    return dataPtr->getIntValue(index, order);
+void Function::setInRecursive(bool state) {
+    inRecursive = state;
 };
 
-float &Function::getFloat(std::vector<int> &index, int order){
-    return dataPtr->getFloatValue(index, order);
+void Function::setListBegin(int begin) {
+    listBegin = begin;
 };
-
-Leaf::Leaf(Data *dataptr): Function(dataptr){};
-
-std::string Leaf::getFunctionName(){
-    return Leaf::functionName;
-}
-
-int Leaf::getArity(){
-    return Leaf::arity;
-};
-
-int Leaf::getOutputNumber(){
-    return Leaf::outputNumber;
-};
-
-void Leaf::execution(){
-};
-
-Max2::Max2(Data *dataptr): Function(dataptr){};
-
-std::string Max2::getFunctionName(){
-    return Max2::functionName;
-}
-
-int Max2::getArity(){
-    return Max2::arity;
-};
-
-int Max2::getOutputNumber(){
-    return Max2::outputNumber;
-};
-
-//implementation of max(a, b)
-void Max2::execution(){
-    int a, b; 
-    if(Const::typeOfBuffer==Int){
-        a = dataPtr->getIntValue(argumentIndex, 1);
-        b = dataPtr->getIntValue(argumentIndex, 2);
-    }   
-    else if(Const::typeOfBuffer==Float){
-        a = dataPtr->getFloatValue(argumentIndex, 1);
-        b = dataPtr->getFloatValue(argumentIndex, 2);  
-    }
-    dataPtr->set(outputIndex, a>b? a:b);
-};
-*/
 
 Swap::Swap(Data *dataptr): Function(dataptr){};
 
@@ -127,6 +56,7 @@ std::string Swap::getFunctionName(){
 }
 
 void Swap::execution(){
+    /*
     int temp; 
     if(Const::typeOfBuffer==Int){
         temp = dataPtr->getIntValue(argumentIndex, 1);
@@ -138,6 +68,7 @@ void Swap::execution(){
         dataPtr->getFloatValue(argumentIndex, 1) = dataPtr->getFloatValue(argumentIndex, 2);
         dataPtr->getFloatValue(argumentIndex, 2) = temp;
     }
+    */
 };
 
 
@@ -145,11 +76,6 @@ int Swap::getArity(){
     return Swap::arity;
 };
     
-int Swap::getOutputNumber(){
-    return Swap::outputNumber;
-};
-
-
 Loop::Loop(Data *dataptr): Function(dataptr){};
 
 std::string Loop::getFunctionName(){
@@ -158,16 +84,6 @@ std::string Loop::getFunctionName(){
 
 void Loop::execution(){
     int temp; 
-    if(Const::typeOfBuffer==Int){
-        temp = dataPtr->getIntValue(argumentIndex, 1);
-        dataPtr->getIntValue(argumentIndex, 1) = dataPtr->getIntValue(argumentIndex, 2);
-        dataPtr->getIntValue(argumentIndex, 2) = temp;
-    }   
-    else if(Const::typeOfBuffer==Float){
-        temp = dataPtr->getFloatValue(argumentIndex, 1);
-        dataPtr->getFloatValue(argumentIndex, 1) = dataPtr->getFloatValue(argumentIndex, 2);
-        dataPtr->getFloatValue(argumentIndex, 2) = temp;
-    }
 };
 
 
@@ -175,6 +91,3 @@ int Loop::getArity(){
     return Loop::arity;
 };
     
-int Loop::getOutputNumber(){
-    return Loop::outputNumber;
-};
